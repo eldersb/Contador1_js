e38b30204a7c4325c1bf75569700e7ca4ed0ddbb
eldersb <122701368+eldersb@users.noreply.github.com>
2024-03-25 08:18:40 -0300

    Atualizações Cronometro

--- cont1.js
+++ cont1.js
@@ -1,48 +1,83 @@
 "use strict";
-let elemento = document.querySelector('#segundo');
-let elemento2 = document.querySelector('#minuto');
-let elemento3 = document.querySelector('#hora');
-
-let segundo = 0;
-let minuto = 0;
-let hora = 0;
-
-let cron;
+ let elemento = document.querySelector('#segundo');
+ let elemento2 = document.querySelector('#minuto');
+ let elemento3 = document.querySelector('#hora');
+ let elemento4 = document.querySelector('#segundo1')
+ let elemento5 = document.querySelector('#minuto1')
+ let elemento6 = document.querySelector('#hora1')
 
 
-function start(){
-    pause();
- cron = setInterval(function() {
-    timer();
-    let cont = elemento.innerHTML = segundo++;
-    let cont2 = elemento2.innerHTML = minuto;
-    let cont3 = elemento3.innerHTML = hora;
-    
-    },1000);
+ let segundo = 0;
+ let minuto = 0;
+ let hora = 0;
 
-}
+ let segundo1 = 0;
+ let minuto1 = 0;
+ let hora1 = 0;
 
-function pause(){
-    clearInterval(cron);
-}
+ let cron;
 
-function reset(){
-    hora = 0;
-    minuto = 0;
-    segundo = 0;
-    document.getElementById('hora').innerText = '0';
-    document.getElementById('minuto').innerText = '0';
-    document.getElementById('segundo').innerText = '0';
+ function start(){
+     pause();
+  cron = setInterval(function() {
+     timer();
+      elemento.innerHTML = segundo++;
+      elemento4.innerHTML = segundo1;
+      elemento2.innerHTML = minuto;
+      elemento5.innerHTML = minuto1;
+      elemento3.innerHTML = hora;
+      elemento6.innerHTML = hora1;
+      elemento2.innerHTML = minuto;
+      elemento3.innerHTML = hora;
 
-}
+     },1000);
 
-function timer() { 
-    if(segundo === 60){
-        segundo = 0;
-       let cont2 = elemento2.innerHTML = minuto++;
-    } if(minuto === 60){
-        minuto = 0;
-        let cont3 = elemento3.innerHTML = hora++;
+ }
+
+ function pause(){
+     clearInterval(cron);
+ }
+
+ function reset(){
+     hora = 0;
+     minuto = 0;
+     segundo = 0;
+     hora1 = 0;
+     minuto1 = 0;
+     segundo1 = 0;
+     document.getElementById('hora').innerText = '0';
+     document.getElementById('minuto').innerText = '0';
+     document.getElementById('segundo').innerText = '0';
+     document.getElementById('hora1').innerText = '0';
+     document.getElementById('minuto1').innerText = '0';
+     document.getElementById('segundo1').innerText = '0';
+
+ }
+
+ function timer() { 
+     if(segundo === 10){
+         segundo = 0;
+          elemento4.innerHTML = segundo1++;
+     } 
+    if(segundo1 === 6){
+      segundo1 = 0;
+      elemento2.innerHTML = minuto++;
     }
-    
-}
+    if(minuto === 10){
+      minuto = 0;
+      elemento5.innerHTML = minuto1++;
+    }
+    if(minuto1 === 6){
+      minuto1 = 0;
+      elemento3.innerHTML = hora++;
+    }
+    if(hora === 10){
+      hora = 0;
+      elemento6.innerHTML = hora1++;
+    }
+    if(hora1 === 2 && hora === 4){
+      reset()
+    }
+   
+
+ }
